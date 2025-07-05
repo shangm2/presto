0b7061489bf8e038703a2ddc1c67ce67695720ab
Shang Ma <shangma@meta.com>
2025-07-04 20:07:38 -0700

    cross class loader boundary

--- presto-main-base/src/main/java/com/facebook/presto/server/PluginManagerUtil.java
+++ presto-main-base/src/main/java/com/facebook/presto/server/PluginManagerUtil.java
@@ -66,6 +66,7 @@ public class PluginManagerUtil
             .add("com.facebook.drift.annotations.")
             .add("com.facebook.drift.TException")
             .add("com.facebook.drift.TApplicationException")
+            .add("io.netty.buffer")
             .build();
 
     private PluginManagerUtil()
